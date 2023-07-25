0a69cfb235184d29e7dc62933f5e84e1399d4eec
OscarZam02062004 <zamoraoscar964@gmail.com>
2023-07-25 10:27:13 -0600

    Datos estaticos

---
+++ DatosEstaticos.py
@@ -0,0 +1,12 @@
+#Datosestaticos
+def listarProductos():
+    print("Lista de productos")
+    print("1. Coca Cola")
+    print("2. Chettos")
+    print("3. Maruchan")
+    print("4. Palomitas")
+    print("5. Gomitas")
+    print("6. Frutas")
+    print("7. Helado")
+    print("8. Juguito")
+    print("9. Sabritas")
--- FuncionPDF.py
+++ FuncionPDF.py
@@ -1,8 +1,17 @@
 from reportlab.pdfgen import canvas
 ruta="C:/Users/zamor/OneDrive/Escritorio/ModularidadPython/Prueba funciones/"
 nombreArchivo=ruta+"reporteGlobal.pdf"
-def generarPDF():
+def generarPDF(listaNombres,listaEdades):
     c = canvas.Canvas(nombreArchivo)
-    c.drawString(200,600,"Hola desde una funcion")
+    xInicial= 200
+    yInicial= 700
+    c.drawString(xInicial,yInicial,"Edad")
+    c.drawString(xInicial+120,yInicial,"Nombre")
+    for i in range(len(listaNombres)):
+        c.setFont('Helvetica',13)
+        yInicial=yInicial-20
+        c.drawString(xInicial,yInicial,listaEdades[i])
+        c.drawString(xInicial+120,yInicial,listaNombres[i])
+        yInicial= yInicial-20
     c.save()
     print("--------Reporte generado---------")
