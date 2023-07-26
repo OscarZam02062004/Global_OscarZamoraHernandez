1315f9f48d6981335ebf21175e192a8b1bf5db4b
OscarZam02062004 <zamoraoscar964@gmail.com>
2023-07-26 10:12:19 -0600

    Funcion QR

--- FuncionPDF.py
+++ FuncionPDF.py
@@ -1,7 +1,10 @@
 from reportlab.pdfgen import canvas
+from FuncionQR import *#----QR----
 ruta="C:/Users/zamor/OneDrive/Escritorio/ModularidadPython/Prueba funciones/"
 nombreArchivo=ruta+"reporteGlobal.pdf"
+nombreQR= ruta+ "miQR.png"#----QR----
 def generarPDF(listaNombres,listaEdades):
+    generarQR(nombreQR,"Hola desde funcion")#----QR----
     c = canvas.Canvas(nombreArchivo)
     xInicial= 200
     yInicial= 700
@@ -12,6 +15,7 @@ def generarPDF(listaNombres,listaEdades):
         yInicial=yInicial-20
         c.drawString(xInicial,yInicial,listaEdades[i])
         c.drawString(xInicial+120,yInicial,listaNombres[i])
+        c.drawImage(nombreQR,200,400,100,100)#----QR----
         yInicial= yInicial-20
     c.save()
     print("--------Reporte generado---------")
--- FuncionQR.py
+++ FuncionQR.py
@@ -1,11 +1,7 @@
 import qrcode
-ruta="C:/Users/zamor/OneDrive/Escritorio/ModularidadPython/Prueba funciones/"
-nombreimagen= ruta+"FuncionQR.png"
-qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_L,box_size=10,border=5)
-# qr.add_data(informacion)
-qr.make(fit=True)
-img=qr.make_image(fill='#000000',back_color=f'#FFFFFF')
-f=open(nombreimagen,"wb")
-f=open(ruta+"miQR.png","wb")
-img.save(f)
-f.close()
+#Informacion del QR
+def generarQR(nombreQR,informacion):
+    img=qrcode.make(informacion)
+    f= open(nombreQR,"wb")
+    img.save(f)
+    f.close()
