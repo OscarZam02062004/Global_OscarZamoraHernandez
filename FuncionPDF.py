01c27f8a34352a20d6ff575c0f787c0f91f1e0ae
OscarZam02062004 <zamoraoscar964@gmail.com>
2023-07-27 09:39:24 -0600

    Ejemplo nombre individual PDF

--- FuncionPDF.py
+++ FuncionPDF.py
@@ -1,9 +1,13 @@
 from reportlab.pdfgen import canvas
 from FuncionQR import *#----QR----
+import datetime
+import locale
+locale.setlocale(locale.LC_ALL,'')
 ruta="C:/Users/zamor/OneDrive/Escritorio/ModularidadPython/Prueba funciones/"
-nombreArchivo=ruta+"reporteGlobal.pdf"
 nombreQR= ruta+ "miQR.png"#----QR----
 def generarPDF(listaNombres,listaEdades):
+    fecha_actual=datetime.datetime.now()
+    nombreArchivo=ruta+"reporteGlobal"+fecha_actual.strftime('%d-%m-%Y-%H-%M-%S')+".pdf"
     generarQR(nombreQR,"Hola desde funcion")#----QR----
     c = canvas.Canvas(nombreArchivo)
     xInicial= 200
