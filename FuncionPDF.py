634e4098df7a900b1e120492336afa2db8e535e2
OscarZam02062004 <zamoraoscar964@gmail.com>
2023-08-09 00:09:17 -0600

    Funciones Generales actualizadas

--- FuncionPDF.py
+++ FuncionPDF.py
@@ -8,18 +8,26 @@ nombreQR= ruta+ "miQR.png"#----QR----
 def generarPDF(listaUsuarios,listaPrecios):
     fecha_actual=datetime.datetime.now()
     nombreArchivo=ruta+"reporteGlobal"+fecha_actual.strftime('%d-%m-%Y-%H-%M-%S')+".pdf"
-    generarQR(nombreQR,"Hola desde funcion")#----QR----
+    ahora=datetime.datetime.now()
+    fecha=ahora.strftime("%Y-%m-%d %H:%M:%S")
+    generarQR=(nombreQR,"Hola desde funcion")#----QR----
     c = canvas.Canvas(nombreArchivo)
     xInicial= 200
     yInicial= 700
-    c.drawString(xInicial,yInicial,"Edad")
-    c.drawString(xInicial+120,yInicial,"Nombre")
-    for i in range(len(listaUsuarios)):
+#----------Datos Generales------------------------
+    c.drawString(420,720,f"Fecha: {fecha} ")
+#---------------------Imagenes------------------------
+    logoDacia= ruta + "LogoDacia.jpg"
+    c.drawImage(logoDacia,200,690,200,200, mask="auto")
+#-------------------Informacion-----------------------
+    c.drawString(50,620,"Estimado(a)")
+    """for i in range(len(listaUsuarios)):
         c.setFont('Helvetica',13)
-        yInicial=yInicial-20
+
+    yInicial=yInicial-20
         c.drawString(xInicial,yInicial,listaPrecios[i])
         c.drawString(xInicial+120,yInicial,listaUsuarios[i])
         c.drawImage(nombreQR,200,400,100,100)#----QR----
-        yInicial= yInicial-20
+        yInicial= yInicial-20"""
     c.save()
     print("--------Reporte generado---------")
--- FuncionesGenerales.py
+++ FuncionesGenerales.py
@@ -1,9 +1,8 @@
 from FuncionPDF import *
 from DatosEstaticos import *
 import os
-listaNombres=[]
-listaCorreo=[]
-listaSueldos=[]
+import time
+import sys
 print("Bienvenidos a la concecionaria Dacia")
 print("---------------")
 def menu():
@@ -15,19 +14,21 @@ def menu():
         print("4. Imprimir Datos de usuarios existentes")
         print("5. Generar PDF")
         print("6. Generar QR")
-        print("7. Listar productos")
-        print("0. Salir")
+        print("7. Garantia por defecto o premium")
+        print("8.Pago en efectivo o en tarjeta")
+        print("9. Salir")
         opcion = input("Elige una opcion ")
         os.system('cls')
         if(opcion=="1"):
-            Usuarios()
+            #Usuarios()
+            Personalizacion_Usuario()
         elif(opcion=="2"):
             pedirDatos()
         elif(opcion=="3"):
             Vendedores()
             input("Presiona Enter para continuar...")
             os.system('cls')
-            inicio()
+            listarCarrosGenerales()
             input("Presiona Enter para continuar...")
             os.system('cls')
             print("Bienvenido a la personalizacion de tu vehiculo")
@@ -45,11 +46,15 @@ def menu():
             input("Presiona Enter para continuar...")
             os.system('cls')
         elif (opcion=="4"):
-            imprimirDatos()
+            Impresion_Usuarios_Existentes()
         elif (opcion=="5"):
             generarPDF(listaUsuarios,listaPrecios)
         elif (opcion=="6"):
-            listarCarros()
+            generarQR()
+        elif(opcion=="7"):
+            Garantia()
+        elif(opcion=="8"):
+            Pago()
         else:
             print("Opcion no valida")
 
@@ -62,11 +67,47 @@ def pedirDatos():
     listaPrecios.append(str(input("Ingresa tu sueldo mensual: ")))
     print("-----------Guardado-------")
 
-def imprimirDatos():
-    
+def Garantia():
+    print("1.Por defecto")
+    print("2.Premium")
+    valor=input("Ingrese la opcion que desee: ")
+    if valor=="1":
+        print("Cada 15,000 km  o 1 año lo que suceda primero")
+    elif valor=="2":
+        print("2 años extendida sin limite de kilometraje")
+    else:
+        print("Opcion no valida")
 
-def inicio():
-    listarCarrosGenerales()
-    
-
-
+def Pago():
+    total=float(input("Ingrese el costo del vehiculo: "))
+    print("1.Pago en efectivo")
+    print("2.Pago con tarjeta")
+    pago=input("¿Como pagaras? ")
+    if pago=="1":
+        PagoEfectivo=float(input("Ingrese la cantidad de efectivo: "))
+        if PagoEfectivo>=total:
+            print("Cobro exitososo disfrute su vehiculo")
+        else:
+            print("No le alcanza jefe")
+    elif pago=="2":
+        mensaje = "Procesando el pago con tarjeta.........."
+        duracion = 5
+        intervalo = 1  
+        parpadeo(mensaje, duracion, intervalo)
+        print("Pago con tarjeta exitoso")
+    else:
+        print("Opcion no valida")
+#---------Datos extras---------------------
+def parpadeo(mensaje, duracion, intervalo):
+    iteraciones = int(duracion / intervalo)
+    for _ in range(iteraciones):
+        sys.stdout.write("\r" + mensaje)
+        sys.stdout.flush()
+        time.sleep(intervalo)
+        
+        sys.stdout.write("\r" + " " * len(mensaje))
+        sys.stdout.flush()
+        time.sleep(intervalo)
+    sys.stdout.write("\r" + mensaje + "\n")
+    sys.stdout.flush()
+                       
